0c776b546a69656beded9c24f9d6b3a6fea4c991
ranjithraja123 <52457937+ranjithraja123@users.noreply.github.com>
2024-07-29 09:52:08 +0530

    Chatapp

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,10 @@ import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
-import { Server } from 'socket.io';
+import path from "path"
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
 
 dotenv.config()
 app.use(express.json())
@@ -18,11 +20,15 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use('/api/users',userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-
-app.get('/',(req,res)=>{
-    res.send('Server is Ready!')
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
+
+// app.get('/',(req,res)=>{
+//     res.send('Server is Ready!')
+// })
 
 
 
